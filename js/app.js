37668f41dc689c5e9075e9381876b804ce2b1b9b
nicolasjhampton <nicolasjhampton@gmail.com>
2015-11-06 20:32:14 -0800

    more palette module work

--- js/app.js
+++ js/app.js
@@ -1,49 +1,61 @@
 "use strict";
 
-var palette = (function(){
+var Palette = (function(){
 
-  return {
+  var palette = {};
 
-    paletteInput: document.getElementsByName('color'),
+  // Palette UI nodes
+  var paletteInput = document.getElementsByName('color');
 
-    currentColorValues: null,
+  // RegEx expression for RGB CSS values
+  var re = /rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/;
 
-    getCheckedRadioBox: function() {
-      for (var i = 0; i < this.paletteInput.length; ++i) {
-        if (this.paletteInput[i].checked === true) {
-          return this.paletteInput[i];
-        }
+  var currentLabelSelected = null;
+
+  // Color last selected in array format: [r,g,b]
+  var currentColorValues = null;
+
+  function getSelectedLabel() {
+    for (var i = 0; i < paletteInput.length; ++i) {
+      if (paletteInput[i].checked === true) {
+        currentLabelSelected = paletteInput[i].nextElementSibling;
+        break;
       }
-    },
-
-    getCurrentLabelStyles: function() {
-      var currentLabel = this.getCheckedRadioBox().nextElementSibling;
-      return window.getComputedStyle(currentLabel);
-    },
-
-    setColor: function() {
-
-      // RegEx expression for RGB CSS values
-      var re = /rgb\((\d{1,3}), (\d{1,3}), (\d{1,3})\)/;
-
-      // Get the CSS stylelist of the label associated with checked input
-      var elStyles = this.getCurrentLabelStyles();
-      // Split the bgcolor string for that element
-      var match = re.exec(elStyles.backgroundColor);
-
-      // Reformat into pure number values, export as object
-      this.currentColorValues = {
-                          "red"  : match[1],
-                          "green": match[2],
-                          "blue" : match[3]
-                        };
-
-      console.log(this.currentColorValues);
     }
-
   }
 
+  function getSelectedLabelStyles() {
+    return window.getComputedStyle(currentLabelSelected);
+  }
+
+  palette.getColor = function() {
+    return "rgb(" + currentColorValues['red'] + ", " + currentColorValues['green'] + ", " + currentColorValues['blue'] + ")";
+  }
+
+  palette.setColor = function() {
+
+    // Store the currently selected label
+    getSelectedLabel();
+    // Get the CSS stylelist of the label associated with checked input
+    var elStyles = getSelectedLabelStyles();
+    // Split the bgcolor string for that element
+    var match = re.exec(elStyles.backgroundColor);
+
+    // Reformat into pure number values, export as object
+    currentColorValues = {
+                        "red"  : match[1],
+                        "green": match[2],
+                        "blue" : match[3]
+                      };
+
+    console.log(currentColorValues);
+  }
+
+  return palette;
+
 }());
+
+
 
 var canvas = (function() {
 
